401a7c4ee2e39422c45fa65542665d0123936cfd
Chaemin <73246476+mini102@users.noreply.github.com>
2022-05-03 15:31:00 +0900

    Update targetNumber.py

--- targetNumber.py
+++ targetNumber.py
@@ -21,8 +21,24 @@ def DFS(numbers,target,depth):
     print("ans")
     return ans
 
+def anotherDFS(numbers, target):
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    dfs(0,0)
+    return answer
+
 def solution(numbers,target):
     answer = 0
     #answer = BFS(numbers,target)
+    #answer = anotherDFS(numbers,target)
     answer = DFS(numbers,target,0)
     return answer
